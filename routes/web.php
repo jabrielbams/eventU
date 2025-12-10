24eabfff98f9fcd12e3a1957d0d5cd805430df45
zulganzs <mfaizulkamal@student.telkomuniversity.ac.id>
2025-12-10 15:40:37 +0700

    progress: ui register

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Localization
+Route::get('lang/{locale}', [\App\Http\Controllers\LocaleController::class, 'setLocale'])->name('lang.switch');
+
+// Authentication
+Route::get('register', [\App\Http\Controllers\AuthController::class, 'showRegister'])->name('register');
+Route::post('register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register.post');
