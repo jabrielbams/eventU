720b7af36353ada0c242e1a7d159ddd5843b869f
Jabriel Bamasena <jabrielbams@gmail.com>
2025-12-15 01:34:07 +0700

    delete: remove unused test_glob.php file

--- test_glob.php
+++
@@ -1,7 +0,0 @@
-<?php
-$base = __DIR__;
-$escaped = str_replace(['[', ']'], ['[[]', '[]]'], $base);
-$path = $escaped . '/database/migrations/*.php';
-echo "Globbing escaped: $path\n";
-$files = glob($path);
-print_r($files);
